b0a71227d98f87dd453b186003352030c0f7747f
Valentin Evteev <stratumforce@protonmail.com>
2017-04-27 20:13:45 +0300

    change default function

--- src/functions.js
+++ src/functions.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
-// printWelcomeMessage
-export default() => console.log('Welcome to the Brain Games!');
+const printWelcomeMessage = () => console.log('Welcome to the Brain Games!');
+export default printWelcomeMessage;
 
 export const getUsername = () => readlineSync.question('\nMay I have your name? ');
 export const printGreeting = username => console.log(`Hello, ${username}!\n`);
