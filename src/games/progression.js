9c2c12f74e7f5597736e66765f06cf2833498fe4
Valentin Evteev <stratumforce@protonmail.com>
2017-04-28 16:47:07 +0300

    add brain-progression game

---
+++ src/games/progression.js
@@ -0,0 +1,29 @@
+import runGame from '../index';
+import { getRandomNumber } from '../functions';
+
+const description = 'What number is missing in this progression?';
+
+const min = -10;
+const max = 10;
+const progressionLength = 10;
+
+const runProgression = () => {
+  const step = getRandomNumber(min, max);
+  const hiddenIndex = getRandomNumber(0, progressionLength - 1);
+
+  const numbers = [getRandomNumber(min, max)];
+  for (let i = 0; i < progressionLength - 1; i += 1) {
+    const newNumber = numbers[i] + step;
+    numbers.push(newNumber);
+  }
+
+  const expectedAnswer = numbers[hiddenIndex];
+  numbers[hiddenIndex] = '..';
+  const question = numbers.join(' ');
+
+  return [question, expectedAnswer.toString()];
+};
+
+export default() => {
+  runGame(description, runProgression);
+};
